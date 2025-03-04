59472d5db600811972361d5d8874235caa2b2eea
Sadeepa2001 <Sadeepa.Kottagodage@study.beds.ac.uk>
2025-03-04 22:27:03 +0530

    first commit

---
+++ game.js
@@ -0,0 +1,16 @@
+// Event listeners for button clicks
+document.getElementById('start-game').addEventListener('click', function() {
+    alert('Start Game button clicked!');
+    // You can add the logic to start the game or redirect to the game screen
+  });
+  
+  document.getElementById('about').addEventListener('click', function() {
+    alert('About button clicked!');
+    // You can add logic to display the game information or redirect to an About page
+  });
+  
+  document.getElementById('leaderboard').addEventListener('click', function() {
+    alert('Leaderboard button clicked!');
+    // You can add logic to show the leaderboard or redirect to a leaderboard page
+  });
+  
